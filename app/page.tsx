f921a6245c858484be90f67b905b0b58e2193461
mariomteixeira <mariomarcio12324@gmail.com>
2023-08-09 22:43:13 -0300

    Desenvolvimento do link para redirecionamento de cada pais usando Next.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
 type Country = {
   name: {
     common: string;
+  }
+
+  flags: {
+    svg: string;
+    alt: string;
   }
 }
 
@@ -16,9 +24,24 @@ export default async function Home() {
   return (
     <section className='grid grid-cols-6 w-full container gap-2 mt-16'>
       {countries.map((country) => (
-        <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl " key={ country.name.common }>
-      <h1 >{country.name.common}</h1>
+        <Link 
+        href={`/country/${country.name.common}`} 
+        key={ country.name.common }
+        >
+        <article 
+        className="h-64 min-w-full p-2 bg-lightgray border-2 rounded-xl hover:border-indigo-400 transition-all hover:shadow-xl"
+        >
+          <div className="relative w-full h-40 p2 overflow-hidden rounded-xl">
+        <Image 
+        src={country.flags.svg} 
+        alt={country.flags.alt} 
+        fill 
+        className="object-cover" />
+        </div>
+
+      <h1 className="font-bold text-xl text-center mt-1 overflow-hidden">{country.name.common}</h1>
       </article>
+      </Link>
       ))}
     </section>
   )
