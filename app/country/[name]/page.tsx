c8b25f965c3d510174d30a75115c613929c6f5ba
mariomteixeira <mariomarcio12324@gmail.com>
2023-08-09 23:09:46 -0300

    Desenvolvendo detalhes de cada pais na rota dinâmica.

--- app/country/[name]/page.tsx
+++ app/country/[name]/page.tsx
@@ -1,5 +1,19 @@
-export default function Country() {
+import type { Country } from "@/app/page"
+
+async function getCountriesName(name: string): Promise<Country> {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`);
+    return (await response.json())[0]; 
+}
+
+
+
+export default async function CountryPage({ 
+    params: { name } 
+    } : { 
+    params: { name: string } }) {
+
+        const country = await getCountriesName(name)
     return (
-        <h1>Country</h1>
+        <h1>{country.name.common}</h1>
     )
 }
